78173e806a7493632e8b9f5532371901a2e68b3c
Daehyeop Ko <daehyeop.ko@gmail.com>
2019-12-09 00:38:23 +0900

    added test python app

--- identidock/app/tests.py
+++ identidock/app/tests.py
@@ -9,8 +9,8 @@ class TestCase(unittest.TestCase):
     def testHelloWorld(self):
         page = self.app.get("/")
         assert page.status_code == 200
-        assert "Hellow Docker! 1,2,3" in str(page.data)
-
+        assert "Hello Docker! 1,2,3" in str(page.data)
+  
     def testGetMainPage(self):
         page = self.app.post("/index", data = dict(name = "Moby Dock"))
         assert page.status_code == 200
@@ -18,7 +18,7 @@ class TestCase(unittest.TestCase):
         assert "Moby Dock" in str(page.data)
 
     def testHtmlEscaping(self):
-        page = self.app.post("/index", data = dict(name = '"><b>TEST</b>!--"'))
+        page = self.app.post("/index", data = dict(name = '"><b>TEST</b><!--"'))
         assert "<b>" not in str(page.data)
 
 if __name__ == "__main__":
--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
-
+echo $PWD
+echo $ENV
 if [ "${ENV}" == "PROD" ]; then
 	echo "Running Production Server"
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /usr/src/app/identidock.py --callable app --stats 0.0.0.0:9191
+elif [ "${ENV}" == "UT" ]; then
+	echo "Running Unit Test"
+	exec python3 "tests.py"
 else
 	echo "Running Development Server"
-	exec python "identidock.py"
+	exec python3 "identidock.py"
 fi
